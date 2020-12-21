7672182c6fa498fead049798ba008748cd0b97fe
Will Parker <wills.parker@outlook.com>
2020-12-21 20:37:26 +0000

    Separate function into sub-functions, ready for multi-threading

--- kernel.py
+++ kernel.py
@@ -1,4 +1,6 @@
 from PIL import Image
+from multiprocessing import Pool
+from itertools import repeat
 import numpy as np
 import math
 import cv2
@@ -11,48 +13,30 @@ class Kernel:
         self.mask.reverse()
         self.padding = padding
 
-    def run (self):
-        ### Initialisation ( i.e. find new image size, get everything in right data type)
-        # oriImg[x|y] stands for original Image [x|y]
+    
+    # Separate function to allow for multi-threading later
+    def processChannel(self, inputArray):
+        # When multithreading, could use pool.starmap
+        # See: https://stackoverflow.com/questions/5442910/how-to-use-multiprocessing-pool-map-with-multiple-arguments
+        # Saves time so I don't have to find these variables 3 times.
         oriImgX, oriImgY = self.inputImage.size
+        oriImg = inputArray
         maskSize=int(math.sqrt(len(self.mask)))
-        oriImg = np.array(self.inputImage.convert("L"))
-
-        ### This is for the RGB stuff. Currently have it working on a single channel at a time
-        # Maybe think about multi-threading each channel? That'd be cool :D
-        r,g,b = self.inputImage.convert("RGB").split()
-        r_channel = np.array(r)
-        oriImg = np.array(g)
-        b_channel = np.array(b)
 
         mask = np.array(self.mask)
         mask = np.reshape(mask, (maskSize, maskSize))
-        mask = np.flipud(np.fliplr(mask))  # may be unnecessary
-
-        ### Find shape of new image (Work on Padding) - may be unnecessary now
-        outImgX = oriImgX
-        outImgY = oriImgY
-    #    if self.padding != 0:
-    #        outImgX =+ self.padding * 2
-    #        outImgY =+ self.padding * 2
-
-        outImg = np.zeros((outImgX, outImgY),np.uint8)
-
-        ### Iterate through image
+        mask = np.flipud(np.fliplr(mask))  
 
         offset = int((maskSize-1)/2)
-        # Pixels[x][y], where x refers to the rowIndex, y refers to columnIndex
+        outImg = np.zeros((oriImgX, oriImgY),np.uint8)
         for rowIndex in range(oriImgX-1):
             for columnIndex in range(oriImgY-1):
-                # In case there was ever any doubt- this is a horrid implementation
                 x = [item for item in (list(range(rowIndex-offset,rowIndex)) + list(range(rowIndex,rowIndex+offset+1))) if item >=0 if item < (oriImgX) ]
                 y = [item for item in (list(range(columnIndex-offset,columnIndex)) + list(range(columnIndex,columnIndex+offset+1))) if item >=0 if item < (oriImgY) ]
                 tmpArray = np.zeros((maskSize,maskSize))
                 newArray = oriImg[np.ix_(y,x)]
                 x_offset, y_offset = 0,0
                 
-                # If there's a 0 in the list, its on the boundary of the image 
-                # & this needs to be accounted for when making the np.ix_ array
                 if 0 in x:
                     x_offset = maskSize - newArray.shape[0]
                 if 0 in y:
@@ -67,9 +51,29 @@ class Kernel:
                 elif value < 0:
                     value = 0
                 outImg[rowIndex][columnIndex] = int(sum(sum(tmpArray * mask)))
+        return outImg
 
-        #returnImage = Image.fromarray(outImg.transpose())
-        returnImage = Image.merge("RGB", (Image.fromarray(r_channel), Image.fromarray(outImg.transpose()), Image.fromarray(b_channel)))
+
+    def run (self):
+        # TODO: Use Checkbox to determine if RGB or not.
+        # Could use 3 / 4 to determine which channels to process.
+        r,g,b = self.inputImage.convert("RGB").split()
+        r_channel = np.array(r)
+        g_channel = np.array(g)
+        b_channel = np.array(b)
+        
+        pro_rchannel = self.processChannel(r_channel).transpose()
+        pro_gchannel = self.processChannel(g_channel).transpose()
+        pro_bchannel = self.processChannel(b_channel).transpose()
+
+        ### Find shape of new image (Work on Padding) - may be unnecessary now
+    #    outImgX = oriImgX
+    #    outImgY = oriImgY
+    #    if self.padding != 0:
+    #        outImgX =+ self.padding * 2
+    #        outImgY =+ self.padding * 2
+
+        returnImage = Image.merge("RGB", (Image.fromarray(pro_rchannel), Image.fromarray(pro_gchannel), Image.fromarray(pro_bchannel)))
         return returnImage
 
     # Debug code
